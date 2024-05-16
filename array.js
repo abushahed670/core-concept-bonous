ca738d58452c7645d8f01f7954b424acabf182b5
abushahed670 <abu.shahed670@gmail.com>
2024-05-16 23:45:36 +0600

    arguments and array

---
+++ array.js
@@ -0,0 +1,16 @@
+// const products =[
+//     {name:'phone', price: 12000},
+//     {name:'laptop', price: 32000}
+// ]
+// products[0];
+// products[1];
+ 
+// products = [56, 57, 87]
+
+const products ={
+    '0' : 56,
+    '1' : 57,
+    '2' : 87
+
+}
+
